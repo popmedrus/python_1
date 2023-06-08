36e443831335e491518bab8efca01f264476c6ee
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-08 13:16:53 +0300

    рефактор модулей логин и создания пользователя - параметризация + изменено название модуля создания пользователя и модуля проверки создания пользователя

--- test_rh10.py
+++ test_rh10.py
@@ -13,32 +13,47 @@ class TestRh10():
     self.driver.quit()
   
   def test_rh10(self):
-    self.driver.get("http://users.bugred.ru/user/login/index.html")
-    self.driver.find_element(By.NAME, "login").click()
-    self.driver.find_element(By.NAME, "login").send_keys("popmedrus@gmail.com")
-    self.driver.find_element(By.NAME, "password").send_keys("12345678")
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(3) .btn").click()
-    self.driver.find_element(By.LINK_TEXT, "Добавить пользователя").click()
-    self.driver.find_element(By.NAME, "noibiz_name").click()
-    self.driver.find_element(By.NAME, "noibiz_name").send_keys("rh13")
-    self.driver.find_element(By.NAME, "noibiz_email").click()
-    self.driver.find_element(By.NAME, "noibiz_email").send_keys("rh13@gmail.com")
-    self.driver.find_element(By.NAME, "noibiz_password").click()
-    self.driver.find_element(By.NAME, "noibiz_password").send_keys("12345678")
-    self.driver.find_element(By.NAME, "noibiz_birthday").click()
-    self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0001-05-07")
-    self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0019-05-07")
-    self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0199-05-07")
-    self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("1995-05-07")
-    self.driver.find_element(By.NAME, "noibiz_gender").click()
-    dropdown = self.driver.find_element(By.NAME, "noibiz_gender")
-    dropdown.find_element(By.XPATH, "//option[. = 'Мужской']").click()
-    self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(2)").click()
-    self.driver.find_element(By.NAME, "act_create").click()
+    self.open_home_page()
+    self.login(login_login="popmedrus@gmail.com", login_password="12345678")
+    self.test_create_user(user_name="rh13", user_email="rh13@gmail.com", user_password="12345678")
+    self.test_check_new_user()
+    self.logout()
+
+  def logout(self):
+    self.driver.find_element(By.LINK_TEXT, "popmedrus").click()
+    self.driver.find_element(By.LINK_TEXT, "Выход").click()
+
+  def test_check_new_user(self):
     self.driver.find_element(By.NAME, "q").click()
     self.driver.find_element(By.NAME, "q").send_keys("rh13")
     self.driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
     self.driver.find_element(By.LINK_TEXT, "Посмотреть").click()
-    self.driver.find_element(By.LINK_TEXT, "popmedrus").click()
-    self.driver.find_element(By.LINK_TEXT, "Выход").click()
+
+  def test_create_user(self, user_name, user_email, user_password):
+    self.driver.find_element(By.LINK_TEXT, "Добавить пользователя").click()
+    self.driver.find_element(By.NAME, "noibiz_name").click()
+    self.driver.find_element(By.NAME, "noibiz_name").send_keys(user_name)
+    self.driver.find_element(By.NAME, "noibiz_email").click()
+    self.driver.find_element(By.NAME, "noibiz_email").send_keys(user_email)
+    self.driver.find_element(By.NAME, "noibiz_password").click()
+    self.driver.find_element(By.NAME, "noibiz_password").send_keys(user_password)
+    self.driver.find_element(By.NAME, "noibiz_birthday").click()
+    #self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0001-05-07")
+    #self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0019-05-07")
+    #self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0199-05-07")
+    #self.driver.find_element(By.NAME, "noibiz_birthday").send_keys("1995-05-07")
+    #self.driver.find_element(By.NAME, "noibiz_gender").click()
+    #dropdown = self.driver.find_element(By.NAME, "noibiz_gender")
+    #dropdown.find_element(By.XPATH, "//option[. = 'Мужской']").click()
+    #self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(2)").click()
+    self.driver.find_element(By.NAME, "act_create").click()
+
+  def login(self, login_login, login_password):
+    self.driver.find_element(By.NAME, "login").click()
+    self.driver.find_element(By.NAME, "login").send_keys(login_login)
+    self.driver.find_element(By.NAME, "password").send_keys(login_password)
+    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(3) .btn").click()
+
+  def open_home_page(self):
+    self.driver.get("http://users.bugred.ru/user/login/index.html")
   
