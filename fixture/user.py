5003a0ced78abc6939449636968147b697316660
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-13 00:25:59 +0300

    два деста по модификации юзера - эмейл и имя

--- fixture/user.py
+++ fixture/user.py
@@ -7,19 +7,29 @@ class UserHelper:
         self.app = app
 
     def check(self, user):
+        self.find(user)
+        self.app.driver.find_element(By.LINK_TEXT, "Посмотреть").click()
+
+    def find(self, user):
         self.app.driver.find_element(By.NAME, "q").click()
         self.app.driver.find_element(By.NAME, "q").send_keys(user.user_name)
         self.app.driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
-        self.app.driver.find_element(By.LINK_TEXT, "Посмотреть").click()
 
     def create(self, user):
         self.app.driver.find_element(By.LINK_TEXT, "Добавить пользователя").click()
-        self.app.driver.find_element(By.NAME, "noibiz_name").click()
-        self.app.driver.find_element(By.NAME, "noibiz_name").send_keys(user.user_name)
-        self.app.driver.find_element(By.NAME, "noibiz_email").click()
-        self.app.driver.find_element(By.NAME, "noibiz_email").send_keys(user.user_email)
-        self.app.driver.find_element(By.NAME, "noibiz_password").click()
-        self.app.driver.find_element(By.NAME, "noibiz_password").send_keys(user.user_password)
+        self.fill_user_form(user)
+        self.app.driver.find_element(By.NAME, "act_create").click()
+
+    def fill_user_form(self, user):
+        if user.user_name is not None:
+            self.app.driver.find_element(By.NAME, "noibiz_name").click()
+            self.app.driver.find_element(By.NAME, "noibiz_name").send_keys(user.user_name)
+        if user.user_email is not None:
+            self.app.driver.find_element(By.NAME, "noibiz_email").click()
+            self.app.driver.find_element(By.NAME, "noibiz_email").send_keys(user.user_email)
+        if user.user_password is not None:
+            self.app.driver.find_element(By.NAME, "noibiz_password").click()
+            self.app.driver.find_element(By.NAME, "noibiz_password").send_keys(user.user_password)
         self.app.driver.find_element(By.NAME, "noibiz_birthday").click()
         self.app.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0001-05-07")
         self.app.driver.find_element(By.NAME, "noibiz_birthday").send_keys("0019-05-07")
@@ -29,5 +39,11 @@ class UserHelper:
         dropdown = self.app.driver.find_element(By.NAME, "noibiz_gender")
         dropdown.find_element(By.XPATH, "//option[. = 'Мужской']").click()
         self.app.driver.find_element(By.CSS_SELECTOR, "option:nth-child(2)").click()
-        self.app.driver.find_element(By.NAME, "act_create").click()
+
+    def modify(self, modified_data):
+        self.app.driver.find_element(By.LINK_TEXT, "Изменить").click()
+        self.fill_user_form(modified_data)
+        self.app.driver.find_element(By.NAME, "act_update").click()
+
+
 
--- model/user.py
+++ model/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, user_name, user_email, user_password):
+    def __init__(self, user_name=None, user_email=None, user_password=None):
         self.user_name = user_name
         self.user_email = user_email
         self.user_password = user_password
