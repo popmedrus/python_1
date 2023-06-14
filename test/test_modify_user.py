d1a1ff776640020d1189c6646c204edd6a2dce3d
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-14 16:44:32 +0300

    оптимизация теста - убрали логин и логаут

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -2,14 +2,10 @@ from model.user import User
 
 
 def test_modify_user_name(app):
-    app.session.login(login_login="popmedrus@gmail.com", login_password="12345678")
     app.user.find(User(user_name="rh22"))
     app.user.modify(User(user_name="new_rh22"))
-    app.session.logout()
 
 
 def test_modify_user_email(app):
-    app.session.login(login_login="popmedrus@gmail.com", login_password="12345678")
     app.user.find(User(user_name="new_rh22"))
     app.user.modify(User(user_email="new_rh22@gmail.com"))
-    app.session.logout()
