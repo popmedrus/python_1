e1e49fb5746d1e28b30633cd8de3fd7dc4d6a10e
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-14 18:55:43 +0300

    Добавил в тест по модификации группы предусловие по созданию группы

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -2,8 +2,9 @@ from model.user import User
 
 
 def test_modify_user_name(app):
-    app.user.find(User(user_name="rh22"))
-    app.user.modify(User(user_name="new_rh22"))
+    if app.user.find(User(user_name="rh23")) is None:
+        app.user.create(User(user_name="rh23", user_email="rh23@gmail.com", user_password="12345678"))
+    app.user.modify(User(user_name="new_rh23"))
 
 
 def test_modify_user_email(app):
