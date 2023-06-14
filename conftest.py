0d035b09a1cb5e01d444307bc0d8ce23914d6609
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-14 17:55:53 +0300

    Интеллектуализировали логин и логаут - добавили проверку предусловия для тестов и автоматизировали логаут чтобы не было эффекта домино при сбоях

--- conftest.py
+++ conftest.py
@@ -2,8 +2,25 @@ from fixture.application import Application
 import pytest
 
 
-@pytest.fixture(scope="session")
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
+fixture = None
+
+
+@pytest.fixture
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.ensure_login(login_login="popmedrus@gmail.com", login_password="12345678")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
--- fixture/session.py
+++ fixture/session.py
@@ -15,3 +15,27 @@ class SessionHelper:
     def logout(self):
         self.app.driver.find_element(By.LINK_TEXT, "popmed").click()
         self.app.driver.find_element(By.LINK_TEXT, "Выход").click()
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        return len(self.app.driver.find_elements(By.LINK_TEXT, "Выход")) > 0
+
+    def is_logged_in_as(self, user_name):
+        return self.app.driver.find_element(By.LINK_TEXT, "popmed").text == '('+user_name+')'
+
+
+    def ensure_login(self, login_login, login_password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(login_login):
+                return
+            else:
+                self.logout()
+        self.login (login_login, login_password)
+
+
+
+
+
