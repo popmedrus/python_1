0e879242920a3225ebd835e1ddf714e6b3aaa298
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-17 23:53:33 +0300

    добавил id в класс User - из за цикла по подсчету чтобы пользователей четко идентифицировать

--- model/user.py
+++ model/user.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, user_name=None, user_email=None, user_password=None):
+    def __init__(self, user_name=None, user_email=None, user_password=None, id=None):
         self.user_name = user_name
         self.user_email = user_email
         self.user_password = user_password
+        self.id = id
