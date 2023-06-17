9b0ad0263d7df4fd735dac7e823712aea676692e
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-06-17 23:54:06 +0300

    переделал modify_user - работал неправильно раньше (else if)

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -2,9 +2,12 @@ from model.user import User
 
 
 def test_modify_user_name(app):
-    if app.user.find(User(user_name="rh23")) is None:
-        app.user.create(User(user_name="rh23", user_email="rh23@gmail.com", user_password="12345678"))
-    app.user.modify(User(user_name="new_rh23"))
+    if app.user.find(User(user_name="rh8")) != 0:
+        app.user.modify(User(user_name="new_rh8"))
+    else:
+        app.user.create(User(user_name="rh8", user_email="rh8@gmail.com", user_password="12345678"))
+        app.user.find(User(user_name="rh8"))
+        app.user.modify(User(user_name="new_rh8"))
 
 
 def test_modify_user_email(app):
