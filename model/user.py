4bcca84c65d310ae05e28d5c8a7b31622f22af66
popmedrus <135801006+popmedrus@users.noreply.github.com>
2023-07-06 00:25:06 +0300

    добавили рандомайзер для создания параметров пользователя

--- model/user.py
+++ model/user.py
@@ -6,7 +6,7 @@ class User:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.user_name)
+        return "%s:%s:%s:%s" % (self.id, self.user_name, self.user_email, self.user_password)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.user_name == other.user_name
